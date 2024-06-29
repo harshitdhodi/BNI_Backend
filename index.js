c0c97671b8194bf79a4cf95831e29b578d7a34b7
harshit b dhodi <harshit.dhodi2108@gmail.com>
2024-06-29 09:24:07 +0530

    done

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = [
-  'http://localhost:5173',  // Development
+  // 'http://localhost:5173',  // Development
   'https://bni-dashboard-new.onrender.com'  // Replace with your actual production domain
 ];
 
